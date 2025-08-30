80e479be6e1bc687623bb4dc4a0c2762a43ddb26
RayssaZsilva <rayssasteele3@gmail.com>
2025-08-29 21:04:13 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
     <div  className="w-[150px] bg-[#c8a2c8]-500 hover:bg-[#c8a2c8]-600 text-[#8B8989] font-bold py-2 px-4 rounded">
       <a href="/produto">Ver produto</a></div>
     </>
+    //npm run dev   rodar no navegador pelo terminal
     //tailwindcss site para estilizar o 7,8
     //estruturar mais a primeira pagina (colocar mais divs, mais textos, criar mais uma rota)
   )

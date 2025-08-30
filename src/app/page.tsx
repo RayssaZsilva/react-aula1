c7439de6bc07e1f4495aa6c60430e31beb041852
RayssaZsilva <rayssasteele3@gmail.com>
2025-08-29 21:02:21 -0300

    atvdd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,16 @@
 
 export default function Home() {
   return (
-   <div style={{backgroundColor:'#ff0000'}}><h1>Pagina teste</h1></div>
+  //  <div style={{ width:'100',backgroundColor:'#c8a2c8 ', padding:'50px',color:'#ffffff'}}><h1>Pagina teste</h1></div>
+  <>
+  
+  <div className="block w-full bg-[#c8a2c8] w-full" ><h1>Pagina teste </h1></div>
+  {/* <div  className="block"><h2>teste</h2></div> */}
+    {/* <div className="block text-[#8B8989]"></div> */}
+    <div  className="w-[150px] bg-[#c8a2c8]-500 hover:bg-[#c8a2c8]-600 text-[#8B8989] font-bold py-2 px-4 rounded">
+      <a href="/produto">Ver produto</a></div>
+    </>
+    //tailwindcss site para estilizar o 7,8
+    //estruturar mais a primeira pagina (colocar mais divs, mais textos, criar mais uma rota)
   )
   }
---
+++ src/app/produto/page.tsx
@@ -0,0 +1,11 @@
+
+export default function produto() {
+  return (
+  <div ><h1>Página produto</h1>
+  
+  </div>
+  
+ 
+    //tailwindcss site para estilizar o 7,8
+  )
+  }
